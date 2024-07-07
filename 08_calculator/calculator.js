46b2b8676af142b98e5fedc00f63c8fabeaf7996
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-07-07 23:15:35 +0530

    Passes the sum tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,11 @@ const subtract = function(a,b) {
 	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((initial,item)=>
+     initial + item
+  ,0);
+  
 };
 
 const multiply = function() {
