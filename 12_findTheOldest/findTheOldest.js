88754092a60751b5e43bc6feac353f41ebd6dd78
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-07-08 20:18:00 +0530

    Complete the last exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let ages = arr.map(item=> {
+        if(item.yearOfDeath!== undefined)
+            return item.yearOfDeath - item.yearOfBirth;
+        else
+           return 2024 - item.yearOfBirth;
+        });
+    let personIndex = highest(ages);
+    return arr[personIndex];
 };
+
+function highest(array){
+    let high = array[0];
+    for(let i=1;i<array.length;i++)
+        if(array[i]>high)
+            high = array[i];
+    return array.indexOf(high);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
